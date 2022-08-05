b346207c421d5ca156c1c627ec40d5d5ccecdcac
John McKinnon <johnmckinnon23@yahoo.com>
2022-08-04 19:23:51 -0600

    commit tree c04bd33f8d713fa9fd33fc2457ff6e9e1cee42db

---
+++ index.js
@@ -0,0 +1,46 @@
+function add(a, b){
+  return a + b;
+}
+ 
+function subtract(a, b){
+  return a - b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+var n = 0
+function increment(n){
+  n++;
+  return n;
+}
+
+var n = 0
+function decrement(n){
+  n--;
+  return n;
+}
+
+function roughScale(x, base) {
+  const parsed = parseInt(x, base);
+  if (isNaN(parsed)) { return 0; }
+  
+}
+
+
+
+
+function makeInt(n){
+  return parseInt("");
+//NaN
+}
+
+function preserveDecimal(n){
+  return parseFloat("");
+  //NaN
+}
